898c1f01a22447b5565a4314c6bb6dc69b7e55bf
Muhammad <mhamad.00257363@gmail.com>
2025-02-17 18:42:33 +0300

    api resposne

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Log;
 abstract class Controller
 {
     use AuthorizesRequests;
@@ -22,6 +23,35 @@ abstract class Controller
         }
 
         return response()->json($response, $code);
+    }
+    public function FailResponse($data = [], $messages = ["Failed"], $status = 500, $custom_code = 500, $validation = false)
+    {
+        $errors = $messages;
+
+        if ($validation) {
+            $errors = [];
+            foreach ($messages->errors() as $key => $messages) {
+                $errors[] = $messages[0];
+            }
+        }
+
+        return response()->json(
+            [
+                'code' => $custom_code != 500 ? (int)$custom_code : (int)$status,
+                'messages' => is_string($errors) ? [$errors] : $errors,
+                'data' => $data
+            ],
+            $status
+        )->header('status', $status);
+    }
+
+    public function error_log($exception)
+    {
+        Log::error([
+            $exception->getFile(),
+            $exception->getLine(),
+            $exception->getMessage()
+        ]);
     }
 
 }
