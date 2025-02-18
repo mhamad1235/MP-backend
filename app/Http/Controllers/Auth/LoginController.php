f62b9565d36ce22d7ab2b5c4d9c6133d839624c0
Muhammad <mhamad.00257363@gmail.com>
2025-02-18 20:16:13 +0300

    login

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Response;
+use App\Http\Requests\Requests\LoginRequest;
+
+class LoginController extends Controller
+{
+    public function login(LoginRequest $request){
+
+        try {
+        $validated = $request->validated();
+
+        if ($validated["credential_type"] == 'email') {
+            $user = User::where('email', $validated["email_or_phone"])->first();
+        } else {
+            $user = User::where('phone', $validated["email_or_phone"])->first();
+        }
+        if (!$user || !Hash::check($validated['password'], $user->password)) {
+            return $this->jsonResponse(false,"Wrong password or email", Response::HTTP_UNAUTHORIZED);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+        $user["user_token"] = $token;
+        return $this->jsonResponse(true,"successful", Response::HTTP_OK, $user);
+    } catch (\Throwable $th) {
+        dd($th->getMessage());
+    }
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\SocialAuthController;
 use App\Http\Controllers\Auth\UserController;
 use App\Http\Controllers\Auth\TwilioController;
+use App\Http\Controllers\Auth\LoginController;
 
 Route::group(["prefix" => "auth"], function () {
     Route::get('/{provider}', [SocialAuthController::class, 'redirectToProvider']);
     Route::get('/{provider}/callback', [SocialAuthController::class, 'handleProviderCallback']);
     Route::post('/send', [TwilioController::class, 'sendOtp']);
     Route::post('/verify', [TwilioController::class, 'verify']);
+    Route::post('/login',[LoginController::class,'login']);
 
 });
 
